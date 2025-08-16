f687bad54390e5bc463dda9c8b4e20f4e89b5375
Harshit Raj Pandey <hrpofficial736@gmail.com>
2025-08-16 19:43:35 +0530

    final

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -63,7 +63,7 @@ const Navigation = ({
           <div className="flex flex-col gap-[12px] lg:gap-[16px] border-primary self-start">
             <div onClick={() => {
                 setShow(false);
-                window.open("/", "auto");
+                window.open("/", "_self");
                 localStorage.setItem("purpose", "main")
               }} className="uppercase font-boskaMedium text-[32px] lg:text-[64px] text-primary cursor-pointer">
               home
@@ -71,7 +71,7 @@ const Navigation = ({
             <div
               onClick={() => {
                 setShow(false);
-               window.open("/towers", "auto")
+               window.open("/towers", "_self")
               }}
               className="uppercase font-boskaMedium text-[32px] lg:text-[64px] text-primary cursor-pointer"
             >
@@ -79,7 +79,7 @@ const Navigation = ({
             </div>
             <div
               onClick={() => {
-                window.open("/amenities", "auto")
+                window.open("/amenities", "_self")
               }}
               className="uppercase font-boskaMedium text-[32px] lg:text-[64px] text-primary cursor-pointer"
             >
@@ -88,7 +88,7 @@ const Navigation = ({
             <div
               onClick={() => {
                 setShow(false);
-                window.open("/", "auto")
+                window.open("/", "_self")
                 localStorage.setItem("purpose", "architect");
                 document.getElementById("about-architect")?.scrollIntoView({ behavior: "smooth" });
               }}
